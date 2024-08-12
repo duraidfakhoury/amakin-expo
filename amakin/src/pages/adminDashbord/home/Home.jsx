b6579fef38742a4fed9d2949e742c82a8b66296b
duraidfakhoury <duraid.fakhoury@gmail.com>
2024-08-12 18:30:21 +0300

    main page for admin

--- amakin/src/pages/adminDashbord/home/Home.jsx
+++ amakin/src/pages/adminDashbord/home/Home.jsx
@@ -1,16 +1,25 @@
+import { useNavigate } from "react-router-dom";
 import "./home.css" ;
 const Home = () => {
+    const navigate = useNavigate();
     return (
         <div className="home">
-            <div className="box box1"></div>
-            <div className="box box2"></div>
-            <div className="box box3"></div>
-            <div className="box box4"></div>
-            <div className="box box5"></div>
-            <div className="box box6"></div>
-            <div className="box box7"></div>
-            <div className="box box8"></div>
-            <div className="box box9"></div>
+            <div className="box box1">
+                <img src="/logo.svg" alt="" />
+            </div>
+            <div className="box box2">
+                <button onClick={()=>navigate('events')}>events</button>
+            </div>
+            <div className="box box3">
+                <button onClick={()=>navigate('createEvent')}>create event</button>
+            </div>
+            <div className="box box4">
+                <button onClick={()=>navigate('categories')}>categories</button>
+            </div>
+            <div className="box box5"> 
+                <button onClick={()=>navigate('SignUp')}>add admin</button>
+            </div>
+            
         </div>
     );
 }
